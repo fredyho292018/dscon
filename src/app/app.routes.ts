2e36f9c75868c1b6677d4c39468ea6cced214100
yuniorcl <yconcepcion@3digits.es>
2019-01-30 08:19:31 +0100

    - Correcciones en las Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,22 @@ import { ServiceComponent } from './components/service/service.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { ContactComponent } from './components/contact/contact.component';
 
-
-
 const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'services/:clasificacion', component: ServicesComponent },
-    { path: 'service/:id', component: ServiceComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: '**', component: PagenotfoundComponent },
-    { path: '', component: HomeComponent },
-    // { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'home', component: HomeComponent },
+  { path: 'services/:clasificacion', component: ServicesComponent },
+  { path: 'service/:id', component: ServiceComponent },
+  { path: 'contact', component: ContactComponent },
+  // { path: '**', component: PagenotfoundComponent },
+  { path: '', component: HomeComponent },
+  {
+    path: '404',
+    component: PagenotfoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/404'
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
-export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
